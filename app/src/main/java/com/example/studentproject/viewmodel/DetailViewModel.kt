43fb2eb387d507504d33cc7f3cba41d92cb0e258
s160423013-Chandra <s160423013@student.ubaya.ac.id>
2026-04-08 08:17:35 +0700

    Update DetailViewModel to inherit from AndroidViewModel and initialize Volley RequestQueue properties

--- app/src/main/java/com/example/studentproject/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/studentproject/viewmodel/DetailViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.studentproject.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.volley.RequestQueue
 import com.example.studentproject.model.Student
 
-class DetailViewModel: ViewModel() {
+class DetailViewModel(application: Application): AndroidViewModel(application) {
     val studentLD = MutableLiveData<Student>()
+    val TAG:String = "Volley Tag"
+    val queue: RequestQueue? = null
+
 
     fun fetch(student: Student) {
 //        val student1 = Student("16055","Nonie","1998/03/28","5718444778",
